b60b91c2002ef5d7cd5cfe7f6eefc4a8005e684b
ohsunho <dhwkdrns99@gmail.com>
2025-06-13 15:28:37 +0900

    feat: 매뉴얼 삭제 API 구현

--- routes/manual.js
+++ routes/manual.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { createManual } from "../controllers/createManual.js";
-import { updateManualName } from "../controllers/updateManualName.js";
 import upload from "../config/multer.js";
+import { updateManualName } from "../controllers/updateManualName.js";
+import { deleteManual } from "../controllers/deleteManual.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 router.post("/manual", verifyToken, upload.array("image"), createManual);
 router.patch("/manual/:manualId", updateManualName);
+router.delete("/manual/:manualId", deleteManual);
 
 export default router;
