fd68cb4852af2e678af5f4ff7e88ac4323e6a85c
osh4357 <tjsgh4357@naver.com>
2025-06-18 13:44:07 +0900

    refactor: redirect 방식 제거 및 기존 API 방식으로 복구 (#27)
    Co-authored-by: ohsunho <dhwkdrns99@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,10 @@ import {
   refreshToken,
   kakaoLogout,
 } from "../controllers/kakaoAuthController.js";
-
-import { kakaoRedirectLogin } from "../controllers/kakaoRedirectLogin.js";
-
 import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/kakao/callback", kakaoRedirectLogin);
 router.post("/kakao/login", kakaoLogin);
 router.post("/kakao/refresh", refreshToken);
 router.post("/kakao/logout", verifyToken, kakaoLogout);
